8c8ef41906eba834e35aef9db6b179dcb1511eef
Jan Stockfisch <jan.stockfisch@flowd.de>
2026-06-03 11:31:51 +0200

    chore: only register form setup when EXT:form is loaded
    EXT:form is an optional (dev) dependency, so guard the flood-protection
    form configuration registration with ExtensionManagementUtility::isLoaded('form').
    Also align ext_emconf version to 0.3.0.

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 defined('TYPO3') or die();
 
-// Register the FloodProtection finisher form configuration the classic way:
-//   plugin.tx_form  -> frontend rendering
-//   module.tx_form  -> backend form editor
-// (TYPO3 v14.2+ deprecates this in favour of Configuration/Form/<Set>/config.yaml
-// auto-discovery, but the TypoScript paths are still honoured until v15.)
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-    'plugin.tx_form.settings.yamlConfigurations.1780296610 = EXT:firewall/Configuration/Form/FloodProtectionSetup.yaml
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('form')) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        'plugin.tx_form.settings.yamlConfigurations.1780296610 = EXT:firewall/Configuration/Form/FloodProtectionSetup.yaml
 module.tx_form.settings.yamlConfigurations.1780296610 = EXT:firewall/Configuration/Form/FloodProtectionSetup.yaml'
-);
+    );
+}
