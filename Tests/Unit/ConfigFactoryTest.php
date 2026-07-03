bacb9f12833051a9536514082cb58e614dcdb1e3
Sascha Egerer <sascha.egerer@flowd.de>
2026-07-03 23:26:22 +0200

    Default the IP resolver to GeneralUtility::getIndpEnv('REMOTE_ADDR')

--- Tests/Unit/ConfigFactoryTest.php
+++ Tests/Unit/ConfigFactoryTest.php
@@ -5,13 +5,41 @@ declare(strict_types=1);
 namespace Flowd\Typo3Firewall\Tests\Unit;
 
 use Flowd\Typo3Firewall\ConfigFactory;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
+use TYPO3\CMS\Core\Http\ServerRequest;
 
 #[CoversClass(ConfigFactory::class)]
 final class ConfigFactoryTest extends TestCase
 {
+    private const string CONFIG_WITHOUT_IP_RESOLVER = <<<'PHP'
+        <?php
+        use Flowd\Phirewall\Config;
+        use Flowd\Phirewall\Store\InMemoryCache;
+        use Psr\EventDispatcher\EventDispatcherInterface;
+
+        return function (EventDispatcherInterface $eventDispatcher): Config {
+            return new Config(new InMemoryCache(), $eventDispatcher);
+        };
+        PHP;
+
+    private const string CONFIG_WITH_IP_RESOLVER = <<<'PHP'
+        <?php
+        use Flowd\Phirewall\Config;
+        use Flowd\Phirewall\Store\InMemoryCache;
+        use Psr\EventDispatcher\EventDispatcherInterface;
+
+        return function (EventDispatcherInterface $eventDispatcher): Config {
+            $config = new Config(new InMemoryCache(), $eventDispatcher);
+            $config->setIpResolver(fn(): string => '198.51.100.9');
+            return $config;
+        };
+        PHP;
+
     #[Test]
     public function getConfigurationPathReturnsPhpFilePath(): void
     {
@@ -43,5 +71,60 @@ final class ConfigFactoryTest extends TestCase
         $patternsPath = ConfigFactory::getPatternsFilePath();
 
         self::assertSame(dirname($configPath), dirname($patternsPath));
+    }
+
+    #[Test]
+    public function fromFileSetsDefaultIpResolverWhenConfigurationDoesNotSetOne(): void
+    {
+        $configPath = $this->writeConfigurationFile(self::CONFIG_WITHOUT_IP_RESOLVER);
+
+        $config = $this->createFactory()->fromFile($configPath);
+
+        self::assertInstanceOf(\Closure::class, $config->getIpResolver());
+    }
+
+    #[Test]
+    public function fromFileKeepsTheIpResolverOfTheConfigurationFile(): void
+    {
+        $configPath = $this->writeConfigurationFile(self::CONFIG_WITH_IP_RESOLVER);
+
+        $config = $this->createFactory()->fromFile($configPath);
+        $ipResolver = $config->getIpResolver();
+
+        self::assertInstanceOf(\Closure::class, $ipResolver);
+        self::assertSame('198.51.100.9', $ipResolver(new ServerRequest()));
+    }
+
+    #[Test]
+    public function fromFileFallsBackToDefaultConfigWithDefaultIpResolver(): void
+    {
+        vfsStream::setup('config');
+
+        $config = $this->createFactory()->fromFile(vfsStream::url('config/missing.php'));
+
+        self::assertInstanceOf(\Closure::class, $config->getIpResolver());
+    }
+
+    private function createFactory(): ConfigFactory
+    {
+        $listenerProvider = new class () implements ListenerProviderInterface {
+            /**
+             * @return list<callable>
+             */
+            public function getListenersForEvent(object $event): iterable
+            {
+                return [];
+            }
+        };
+
+        return new ConfigFactory(new EventDispatcher($listenerProvider));
+    }
+
+    private function writeConfigurationFile(string $content): string
+    {
+        $vfsStreamDirectory = vfsStream::setup('config');
+        vfsStream::newFile('phirewall.php')->at($vfsStreamDirectory)->setContent($content);
+
+        return vfsStream::url('config/phirewall.php');
     }
 }
