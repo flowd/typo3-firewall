ea12c4d6bc0b42cc4a63ec9f1d7587cf93ef579d
Sascha Egerer <sascha.egerer@flowd.de>
2026-09-01 23:58:47 +0200

    Load the bundled vendor autoloader in classic mode

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 defined('TYPO3') or die();
 
+if (!defined('TYPO3_COMPOSER_MODE') || !TYPO3_COMPOSER_MODE) {
+    require_once __DIR__ . '/Resources/Private/Php/ComposerLibraries/vendor/autoload.php';
+}
+
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('form')) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         'plugin.tx_form.settings.yamlConfigurations.1780296610 = EXT:firewall/Configuration/Form/FloodProtectionSetup.yaml
